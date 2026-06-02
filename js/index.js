a4303ab8e372703fd7de98123c4ce7b7c6dfbf83
giannip <perkinsgianni@gmail.com>
2026-06-02 19:57:26 -0400

    6/2, API fetch completed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 let today = new Date();
 let thisYear = today.getFullYear();
 
-/////////////////////////////
-
 let footer = document.createElement("footer");
 let copyright = document.createElement("p");
 copyright.innerHTML = `&copy; ${thisYear} Gianni Perkins`;
@@ -10,7 +8,7 @@ document.body.appendChild(footer).appendChild(copyright);
 
 /////////////////////////////
 
-let skills =  ["HTML", "JavaScript", "CSS", "GitHub"];
+let skills =  ["HTML", "CSS", "JavaScript", "GitHub"];
 let skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul");
 
@@ -48,3 +46,33 @@ messageForm.addEventListener("submit", function (event) {
     messageList.appendChild(newMessage);
     messageForm.reset();
 });
+
+/////////////////////////////
+
+fetch('https://api.github.com/users/perkinsgianni/repos')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Request failed');
+        }
+    return response.json(); // parse response as JSON
+    })
+    .then(repositories => {
+        console.log(repositories); // assign response to repositories, log
+
+        let projectSection = document.getElementById("projects");
+        let projectList = projectSection.querySelector("ul");
+
+        for (let i = 0; i < repositories.length; i++) {
+            // let project = document.createElement("li");
+            // project.textContent = repositories[i].name;
+            // projectList.appendChild(project);
+
+            let project = document.createElement("a");
+            project.href = `https://github.com/perkinsgianni/${repositories[i].name}`;
+            project.textContent = repositories[i].name;
+            projectList.appendChild(project);
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred:', error);
+    })
